30c6c262793dd125fc7053608fbc2c965ec0360d
Jonathan Packard <jwpackard@duck.com>
2025-06-08 14:39:44 -0400

    Switched from docker to podman (docker-containers to virtualization.oci-containers)

--- nixos/pihole.nix
+++ nixos/pihole.nix
@@ -1,11 +1,12 @@
-{ lib, config, pkgs, inputs, vars, unstable, ... }:
+{ lib, config, pkgs, inputs, vars, ... }:
 
 {
-
   # pihole docker container (DNS server with ad and tracker blocking)
   # Ref: https://github.com/NixOS/nixpkgs/issues/61617#issuecomment-500793160
-  docker-containers.pihole = {
+
+  virtualisation.oci-containers.containers.pihole = {
     image = "pihole/pihole:latest";
+    # pull = "always"; # Ensures the image is always pulled - needs 25.05??
     ports = [
       "53:53/tcp"
       "53:53/udp"
@@ -16,12 +17,10 @@
       "/var/lib/pihole/:/etc/pihole/"
       "/var/lib/dnsmasq.d:/etc/dnsmasq.d/"
     ];
-    extraDockerOptions = [
+    extraOptions = [
       "--cap-add=NET_ADMIN"
       "--dns=127.0.0.1"
       "--dns=1.1.1.1"
     ];
-    workdir = "/var/lib/pihole/";
   };
-
 }
